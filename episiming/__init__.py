b2275cc1023b0417b2f0a693a50a12f79dbd85e5
Ricardo Rosa <rmsrosa@gmail.com>
2020-07-20 23:29:23 -0300

    reorganizing scenes in a separate folder

--- episiming/__init__.py
+++ episiming/__init__.py
@@ -7,8 +7,11 @@ EpiSiming is a package for the simulation of epidemics.
 According to [Wikipedia](https://en.wikipedia.org/wiki/Siming_(deity)),
 'Siming' refers to a Chinese deity that makes fine adjustments to 
 the fate of humankind.
+
+This package has tools to simulate the evolution of an epidemic by
+compartimental and agent-based models.
 """
 
-from . import scenes
+from .scenes import scenes
 
-name = 'episiming'
+name = 'EpiSiming'
---
+++ episiming/scenes/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# __init__.py
+#
+#"""
+#Scenes for the EpiSiming epidemic simulation package.
+#"""
+
+#from . import scenes
+
+#name = 'EpiSiming Scenes'
