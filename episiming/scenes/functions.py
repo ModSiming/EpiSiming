23aa491bba367453e7da7034bc0400a36ea89941
rodlcp <rodrigo.lima1996@gmail.com>
2020-09-23 10:50:46 -0300

    age generation

--- episiming/scenes/functions.py
+++ episiming/scenes/functions.py
@@ -444,3 +444,28 @@ def get_age_fractions(age_groups: List[int],
                     casos.append(infectados)
                     j += 1
         return np.hstack(casos)
+
+def weights(list):
+    return list / sum(list)
+
+def gen_age(residencias, age_fractions, age_range):
+    n = len(residencias)
+    n_ages = len(age_fractions)
+    ages = -1 * np.ones(n, int)
+    max_people = np.ceil(n * age_fractions / sum(age_fractions))
+
+    aux = [np.arange(n, dtype=int)[residencias == i] for i in range(max(residencias)+1)]
+
+    for i in aux:
+        ages[i[0]] = np.random.choice(range(n_ages), p=weights(max_people * age_range[0]))
+        max_people[ages[i[0]]] -= 1
+        if len(i) == 2:
+            ages[i[2]] = np.random.choice(range(n_ages), p=weights(max_people * (age_range[1] if np.random.rand() > 0.9 else age_range[0])))
+            max_people[ages[i[2]]] -= 1
+
+    for i in aux:
+        if len(i) > 2:
+            for j in range(1, len(i)):
+                ages[i[j]] = np.random.choice(range(n_ages), p=weights(max_people))
+                max_people[ages[i[j]]] -= 1
+    return ages
