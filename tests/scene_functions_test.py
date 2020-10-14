db49fca666ff90eea832ba47dc1fbd1bb246080c
Gil <gilsmneto@gmail.com>
2020-10-13 22:20:31 -0300

    Functions test update, to match the correct type output

--- tests/scene_functions_test.py
+++ tests/scene_functions_test.py
@@ -9,6 +9,6 @@ Test it with `python -m tests.functions_test` from the repo root directory.
 
 import episiming.scenes.functions as ef
 
-assert(ef.get_age_fractions([0, 2], [0.4, 0.6], 4, 'constant')
+assert(list(ef.get_age_fractions([0, 2], [0.4, 0.6], 4, 'constant'))
        == [0.2, 0.2, 0.3, 0.3]), \
        "Function `get_age_fractions` failed to properly generate age fractions"
